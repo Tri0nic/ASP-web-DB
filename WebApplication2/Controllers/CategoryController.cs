0f6a552941597da64a605cdd89ba631a60754b2f
Tri0nic <androyd0007@yandex.ru>
2023-07-29 03:59:29 +0300

    Добавьте файлы проекта.

---
+++ WebApplication2/Controllers/CategoryController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Data;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+    
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        // Кнопка Create New Category
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)//Здесь obj - просто название экземпляра модели Category
+        {
+            if (obj.Name == obj.DisplayOrder.ToString()) //если оба поля совпали, то выдаем ошибку
+            {
+                // Изменяем текст вывода ошибки в поле Name
+                ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name.");
+            }    
+            if (ModelState.IsValid) //проверяем чтобы не было пустых ячеек
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // Кнопка Edit
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id==null || id==0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.FirstOrDefault(c => c.Id == id); // Выделяем строку, соответствующую данному id
+            
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString()) //если оба поля совпали, то выдаем ошибку
+            {
+                // Изменяем текст вывода ошибки в поле Name
+                ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid) //проверяем чтобы не было пустых ячеек
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // Кнопка Delete
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.FirstOrDefault(c => c.Id == id); // Выделяем строку, соответствующую данному id
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var categoryFromDb = _db.Categories.FirstOrDefault(c => c.Id == id); // Выделяем строку, соответствующую данному id
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(categoryFromDb);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplication2/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DisplayName("Display Order")] // Изменяет отображаемое на сайте имя в ошибке
+        [Range(1, 10000, ErrorMessage = "The entered number must be between 1 and 10000!")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
